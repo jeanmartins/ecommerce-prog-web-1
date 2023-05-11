676e4c59e4620eef5f797bfbc66f47e8bc539ab0
Larissa Araújo <lariaraujo201@gmail.com>
2023-05-11 09:39:37 -0300

    add login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import './Login.css';
 
 export function Login() {
     return (
-        <section className="login">
-            <h1>Faça Login</h1>
-            
-            <div className="div-inputs">
-                <div className="label-required">
-                    <label for="email">Email</label>
-                    <label className="asterisco">*</label>
+        <section className="section-form div-column">
+            <form onSubmit={() =>{}}>
+                <h1>Faça Login</h1>
+                
+                <div className="div-inputs div-column">
+                    <div className="label-input">
+                        <label for="email">Email</label>
+                        <label className="required">*</label>
+                    </div>
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder="Ex: alifernandes@gmail.com"
+                    />
+
+                    <div className="label-input">
+                        <label for="email">Senha</label>
+                        <label className="required">*</label>
+                    </div>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Não escreva “123”"
+                    />
                 </div>
-                <input
-                    id="email"
-                    type="email"
-                    placeholder="digite seu email"
-                />
-            </div>
+
+                <div className="div-buttons div-column">
+                    <button className="yellow" type="submit">Entrar</button>
+                    <Link to="/cadastro">Não tenho conta</Link>
+                </div>
+            </form>
         </section>
     );
 }
