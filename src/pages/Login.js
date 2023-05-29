bf042eca674d9a89d0f0c99a15e8e05a5c6b589e
jean_martins <jean.carlos.martins00@gmail.com>
2023-05-28 21:48:04 -0300

    added authorization token in requests

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
 import { Input } from "../components/Input";
-
 import './LoginSignUp.css';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; 
+import api from "../services/api";
 
 export function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('/ap1/v1/user/login', {
+                email,
+                senha: password
+            });
+
+            if(response.data.errorMessage != null){
+                return console.error('Error logging in:', response.data.errorMessage);
+            }
+
+            console.log('User logged in successfully:', response.data);
+            sessionStorage.setItem("jwt", response.data.token)
+            
+            setEmail('');
+            setPassword('');
+        } catch (error) {
+            console.error('Error logging in:', error.response.data);
+        }
+    };
+
     return (
         <section className="section-form div-column">
-            <form onSubmit={() =>{}}>
+            <form onSubmit={handleSubmit}>
                 <h1>Faça Login</h1>
-                
+
                 <Input 
                     id="email"
                     type="email"
                     label="Email"
                     placeholder="Ex: alifernandes@gmail.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
 
                 <Input 
@@ -23,6 +50,8 @@ export function Login() {
                     type="password"
                     label="Senha"
                     placeholder="Não escreva 123"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
 
                 <div className="div-buttons div-column">
--- src/services/api.js
+++ src/services/api.js
@@ -2,15 +2,18 @@ import axios from "axios";
 // import { getToken } from "./auth";
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:3030'
+    baseURL: 'http://localhost'
 });
 
-// api.interceptors.request.use(async config => {
-//     const token = getToken();
+api.interceptors.request.use(async config => {
+    const token = getToken();
 
-//     if (token) config.headers.Authorization = `Bearer ${token}`;
+    if (token) config.headers.Authorization = `Bearer ${token}`;
 
-//     return config;
-// });
+    return config;
+});
 
+function getToken() {
+    return sessionStorage["jwt"] ?? null
+}
 export default api;
