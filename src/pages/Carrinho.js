c35b165966b87de82c0c0d67c4f8ed78f3111515
jean_martins <jean.carlos.martins00@gmail.com>
2023-07-05 01:55:35 -0300

    update carrinho de compras

--- src/pages/Carrinho.js
+++ src/pages/Carrinho.js
@@ -8,116 +8,183 @@ import plus from '../assets/plus.svg';
 import './Carrinho.css';
 
 export function Carrinho() {
-    const [meusProdutos, setMeusProdutos] = useState([]);
+  const [meusProdutos, setMeusProdutos] = useState([]);
+  const [totalPedido, setTotalPedido] = useState(0);
 
-    const getMeusProdutos = () => setMeusProdutos([
-        {
-            img: "https://cobasi.vteximg.com.br/arquivos/ids/939212/racao-golden-formula-caes-adultos-racas-pequenas-frango-arroz-mini-bits-3626279-1kg.jpg?v=638127640641870000",
-            nome: "Ração Golden Fórmula Mini Bits para Cães Adultos de Porte Pequeno Sabor Frango e Arroz",
-            quantidade: 3,
-            preco: "149,90",
-        },
-        {
-            img: "https://cobasi.vteximg.com.br/arquivos/ids/939212/racao-golden-formula-caes-adultos-racas-pequenas-frango-arroz-mini-bits-3626279-1kg.jpg?v=638127640641870000",
-            nome: "Ração Golden Fórmula Mini Bits para Cães Adultos de Porte Pequeno Sabor Frango e Arroz",
-            quantidade: 1,
-            preco: "149,90",
-        },
-    ]);
+  const getMeusProdutos = () => {
+    const carrinho = JSON.parse(localStorage.getItem('carrinho')) || {};
+    const produtos = Object.keys(carrinho).map((produtoId) => ({
+      id: produtoId,
+      quantidade: carrinho[produtoId].quantidade,
+      foto: carrinho[produtoId].foto,
+      preco: carrinho[produtoId].preco,
+      descricao: carrinho[produtoId].descricao
+    }));
+    setTotalPedido(produtos.reduce((total, produto) => {
+      return total + produto.quantidade * produto.preco;
+    }, 0));
+    setMeusProdutos(produtos);
+  };
 
-    const totalPedido = 'R$599,60';
+  const updateLocalStorage = (produtos) => {
+    const carrinho = {};
+    produtos.forEach((produto) => {
+      carrinho[produto.id] = {
+        quantidade: produto.quantidade,
+        foto: produto.foto,
+        preco: produto.preco,
+        descricao: produto.descricao
+      };
+    });
+    setTotalPedido(produtos.reduce((total, produto) => {
+        return total + produto.quantidade * produto.preco;
+      }, 0));
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+  };
 
-    useEffect(() => {
-        getMeusProdutos();
-    }, []);
+  const handleDecrement = (produtoId) => {
+    setMeusProdutos((prevProdutos) => {
+      const updatedProdutos = prevProdutos.map((produto) => {
+        if (produto.id === produtoId && produto.quantidade > 1) {
+          return { ...produto, quantidade: produto.quantidade - 1 };
+        }
+        return produto;
+      });
+      updateLocalStorage(updatedProdutos);
+      return updatedProdutos;
+    });
+  };
 
-    return (
-        <section className="section-carrinho div-column">
-            <div className="div-row">
-                <h1>Carrinho de compras</h1>
+  const handleIncrement = (produtoId) => {
+    setMeusProdutos((prevProdutos) => {
+      const updatedProdutos = prevProdutos.map((produto) => {
+        if (produto.id === produtoId) {
+          return { ...produto, quantidade: produto.quantidade + 1 };
+        }
+        return produto;
+      });
+      updateLocalStorage(updatedProdutos);
+      return updatedProdutos;
+    });
+  };
+
+  const handleRemoveProduto = (produtoId) => {
+    setMeusProdutos((prevProdutos) => {
+      const updatedProdutos = prevProdutos.filter((produto) => produto.id !== produtoId);
+      updateLocalStorage(updatedProdutos);
+      return updatedProdutos;
+    });
+  };
+
+  useEffect(() => {
+    getMeusProdutos();
+  }, []);
+
+  const isCarrinhoVazio = meusProdutos.length === 0;
+  const isAutenticated =  sessionStorage.getItem('jwt') !== null;
+
+  return (
+    <section className="section-carrinho div-column">
+      <div className="div-row">
+        <h1>Carrinho de compras</h1>
+      </div>
+
+      <div className="section-content">
+        <div className="info-pedido div-column">
+          <div className="item-content">
+            <div className="title">
+              <span>Meus produtos</span>
+              <button
+                className="yellow"
+                onClick={() => {
+                  setMeusProdutos([]);
+                  updateLocalStorage([]);
+                }}
+              >
+                Limpar carrinho
+              </button>
             </div>
+            <div className="group-item-pedido">
+              <div className="lista-produto div-column">
+                {meusProdutos.map((produto) => (
+                  <div className="item-produto carrinho">
+                    <div className="infos-produto">
+                      <img src={produto.foto} alt="Imagem produto" />
 
-            <div className="section-content">
-                <div className="info-pedido div-column">
-                    <div className="item-content">
-                        <div className="title">
-                            <span>Meus produtos</span>
-                            <button
-                                className="yellow"
-                                onClick={() => {}}
-                            >
-                                Limpar carrinho
-                            </button>
-                        </div>
-                        <div className="group-item-pedido">
-                            <div className="lista-produto div-column">
-                                {meusProdutos.map(produto => (
-                                    <div className="item-produto carrinho">
-                                        <div className="infos-produto">
-                                            <img src={produto.img} alt="Imagem produto"/>
-
-                                            <div className="dados">
-                                                <label>{produto.nome}</label>
-                                            </div>
-                                        </div>
-                                        
-                                        <div className="acoes">
-                                            <div className="qtd">
-                                                <button
-                                                    className="icon-button"
-                                                    onClick={() => {}}
-                                                >
-                                                    <img className="icon" src={minus} alt="diminuir quantidade" />
-                                                </button>
-                                               
-                                                <span>{produto.quantidade}</span>
-
-                                                <button
-                                                    className="icon-button"
-                                                    onClick={() => {}}
-                                                >
-                                                    <img className="icon" src={plus} alt="aumentar quantidade" />
-                                                </button>
-                                            </div>
-
-                                            
-                                            <button 
-                                                className="icon-button"
-                                                onClick={() => {}}
-                                            >
-                                                <img className="icon trash" src={trash} alt="aumentar quantidade" />
-                                            </button>
-
-                                            <span className="preco-produto">R${produto.preco}</span>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="resumo-pedido item-content">
-                    <h3>Resumo do pedido</h3>
-
-                    {meusProdutos.map(produto => (
-                        <label>{produto.quantidade}x - {produto.nome}</label>
-                    ))}
-                    
-                    <hr />
-                    <div className="total">
-                        <label>Total</label>
-                        <label className="total-price">{totalPedido}</label>
+                      <div className="dados">
+                        <label>{produto.descricao}</label>
+                      </div>
                     </div>
 
-                    <NavLink 
-                        className="btn-finalizar"
-                        to="/finalizarCompra"
-                    >
-                        Finalizar compra
-                    </NavLink>
-                </div>
+                    <div className="acoes">
+                      <div className="qtd">
+                        <button
+                          className="icon-button"
+                          onClick={() => handleDecrement(produto.id)}
+                        >
+                          <img className="icon" src={minus} alt="diminuir quantidade" />
+                        </button>
+
+                        <span>{produto.quantidade}</span>
+
+                        <button
+                          className="icon-button"
+                          onClick={() => handleIncrement(produto.id)}
+                        >
+                          <img className="icon" src={plus} alt="aumentar quantidade" />
+                        </button>
+                      </div>
+
+                      <button
+                        className="icon-button"
+                        onClick={() => handleRemoveProduto(produto.id)}
+                      >
+                        <img className="icon trash" src={trash} alt="remover produto" />
+                      </button>
+
+                      <span className="preco-produto">R${produto.preco}</span>
+                    </div>
+                  </div>
+                ))}
+                {isCarrinhoVazio && (
+            <p>O carrinho está vazio.</p>
+                )}
+              </div>
             </div>
-        </section>
-    );
+          </div>
+        </div>
+
+        <div className="resumo-pedido item-content">
+          <h3>Resumo do pedido</h3>
+
+          {meusProdutos.map((produto) => (
+            <label>{produto.quantidade}x - {produto.descricao}</label>
+          ))}
+
+          {isCarrinhoVazio && (
+            <p>O carrinho está vazio.</p>
+          )}
+
+          <hr />
+          <div className="total">
+            <label>Total</label>
+            <label className="total-price">{totalPedido}</label>
+          </div>
+
+          <button
+            className="btn-finalizar"
+            to="/finalizarCompra"
+            disabled={isCarrinhoVazio || !isAutenticated}
+            title={
+                isCarrinhoVazio
+                  ? 'Seu carrinho está vazio'
+                  : isAutenticated ? 'Finalizar compra' : 'Faça login para finalizar a compra'
+              }
+          >
+            Finalizar compra
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/pages/HomeCliente.js
+++ src/pages/HomeCliente.js
@@ -1,60 +1,128 @@
 import React, { useEffect, useState } from "react";
 import './HomeCliente.css';
 import api from "../services/api";
+import minus from '../assets/minus.svg';
+import plus from '../assets/plus.svg';
 
 export function HomeCliente() {
+    const [successMessage, setSuccessMessage] = useState("");
     const [meusProdutos, setMeusProdutos] = useState([]);
+    const [quantidades, setQuantidades] = useState({});
 
     const getMeusProdutos = () => {
-
+        setMeusProdutos([]);
         async function getProducts() {
             const response = await api.get(`/api/v1/product/get`);
-            response.data.forEach(produto => { setMeusProdutos(prevProdutos => [...prevProdutos, produto])})
+            response.data.forEach(produto => {
+                setMeusProdutos(prevProdutos => [...prevProdutos, produto]);
+                setQuantidades(prevQuantidades => ({ ...prevQuantidades, [produto.id]: 0 }));
+            });
         }
         getProducts();
     }
-       
-
 
     useEffect(() => {
         getMeusProdutos();
     }, []);
 
+    const adicionarQuantidade = (produto) => {
+        setQuantidades(prevQuantidades => ({
+            ...prevQuantidades,
+            [produto.id]: prevQuantidades[produto.id] + 1
+        }));
+    };
+
+    const retirarQuantidade = (produto) => {
+        setQuantidades(prevQuantidades => ({
+            ...prevQuantidades,
+            [produto.id]: Math.max(prevQuantidades[produto.id] - 1, 0)
+        }));
+    };
+
+
+const adicionarAoCarrinho = (produto) => {
+    const quantidade = quantidades[produto.id];
+
+    const carrinhoAtual = JSON.parse(localStorage.getItem('carrinho')) || {};
+
+    if (carrinhoAtual.hasOwnProperty(produto.id)) {
+        carrinhoAtual[produto.id].quantidade += quantidade;
+    } else {
+        carrinhoAtual[produto.id] = {
+            quantidade: quantidade,
+            foto: produto.foto,
+            preco: produto.preco,
+            descricao: produto.descricao
+        };
+    }
+    localStorage.setItem('carrinho', JSON.stringify(carrinhoAtual));
+    setSuccessMessage("Produto adicionado ao carrinho com sucesso!");
+            setTimeout(() => {
+                setSuccessMessage("");
+                }, 2000);  
+
+};
+
     return (
         <section className="section-admin div-column">
+              {successMessage && (
+          <div className="alert alert-success mt-3 text-center fixed-top" role="alert">
+          {successMessage}
+          </div>
+        )}
             <div className="div-row">
-            <h1 style={{
-  color: '#6283FA',
-  fontSize: '24px',
-  fontWeight: 'bold'
-}}>Produtos mais pedidos</h1>
-
+                <h1 style={{
+                    color: '#6283FA',
+                    fontSize: '24px',
+                    fontWeight: 'bold'
+                }}>Produtos mais pedidos</h1>
             </div>
 
-          
-
             <section className="section-content">
-                    <div className="cliente-produto div-row">
-                        {meusProdutos.map(produto => ( produto.quantidade > 0 &&
-                            <div className="item-produto">
+                <div className="cliente-produto div-row">
+                    {meusProdutos.map(produto => (
+                        produto.quantidade > 0 && (
+                            <div className="item-produto" key={produto.id}>
                                 <div className="infos-produto">
-                                    <img src={produto.foto} alt="Imagem produto"/>
+                                    <img src={produto.foto} alt="Imagem produto" />
 
                                     <div className="dados">
-
-                                        
-                                        <label  className="label-with-ellipsis" title={produto.descricao}>{produto.descricao}</label>
-
+                                        <label className="label-with-ellipsis" title={produto.descricao}>{produto.descricao}</label>
                                         <div>
-
                                             <span className="preco"> R${produto.preco}</span>
+                                        </div>
+                                        <div className="acoes">
+                                            <div className="qtd">
+                                                <button
+                                                    className="icon-button"
+                                                    onClick={() => retirarQuantidade(produto)}
+                                                >
+                                                    <img className="icon" src={minus} alt="diminuir quantidade" />
+                                                </button>
+                                                <span>{quantidades[produto.id]}</span>
+                                                <button
+                                                    className="icon-button"
+                                                    onClick={() => adicionarQuantidade(produto)}
+                                                >
+                                                    <img className="icon" src={plus} alt="aumentar quantidade" />
+                                                </button>
+                                            </div>
+
+                                            <button
+                                                className="carrinhoAdd"
+                                                type="submit"
+                                                onClick={() => adicionarAoCarrinho(produto)}  
+                                            >
+                                            Adicionar
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        ))}
-                    </div>
-        </section>
+                        )
+                    ))}
+                </div>
+            </section>
         </section>
     );
 }
