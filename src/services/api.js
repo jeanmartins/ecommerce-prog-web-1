b5e9e480675faa0342d9b5b0cd5d59d956300a93
jean_martins <jean.carlos.martins00@gmail.com>
2023-07-06 02:17:36 -0300

    Update CSS to hide when product name is too big

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://localhost:44383'
+    baseURL: 'http://localhost'
 });
 
 api.interceptors.request.use(async config => {
